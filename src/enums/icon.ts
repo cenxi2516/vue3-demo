33e41cb52bc3f1ad5fb2bf8f6f4910b2b5a98ef0
李顺祥 <shunxiang.li@delonixmail.com>
2024-04-10 16:07:20 +0800

    feat(icon): add icons and icon enum

---
+++ src/enums/icon.ts
@@ -0,0 +1,31 @@
+// 枚举值：svg图文件名
+export enum PMSIconsEnum {
+  AR_MANAGEMENT = 'ar_management', // AR管理
+  DOCUMENT_DEFINITION = 'document_definition', // 单据定义
+  FRONT_DESK_ACCOUNTS = 'front_desk_accounts', // 前台账务
+  PMS_IMPLEMENTATION = 'pms_implementation', // PMS实施
+  FLYING_PIG = 'flying_pig', // 飞猪
+  INVOICE_MANAGEMENT = 'invoice_management', // 发票管理
+  CUSTOMER_MANAGEMENT = 'customer_management', // 客户管理
+  HELP_CENTER_MANAGEMENT = 'help_center_management', // 帮助中心管理
+  EMERGENCY_REPORT = 'emergency_report', // 应急报表
+  HOUSE_PRICE_MANAGEMENT = 'house_price_management', // 房价管理
+  REPORT_CENTER = 'report_center', // 报表中心
+  REPORT_RECONSTRUCTION = 'report_reconstruction', // 报表重建
+  CTRIP = 'ctrip', // 携程
+  BREAKFAST_MANAGEMENT = 'breakfast_management', // 早餐管理
+  AUTHORITY_MANAGEMENT = 'authority_management', // 权限管理
+  MESSAGE_CENTER = 'message_center', // 消息中心
+  CHANNEL_INVENTORY = 'channel_inventory', // 聚道库存
+  BAIDAK_CARD_RESERVATION = 'baidak_card_reservation', // 百达卡预定
+  SIGNATURE_MANAGEMENT = 'signature_management', // 签名管理
+  MEITUAN = 'meituan', // 美团
+  MARKETING_ACTIVITIES = 'marketing_activities', // 营销活动
+  LOST_ITEMS = 'lost_items', // 遗失物品
+  HOTEL_CONFIGURATION = 'hotel_configuration', // 酒店配置
+  ACCESS_CARD_MANAGEMENT = 'access_card_management', // 门卡管理
+  DOOR_LOCK_MANAGEMENT = 'door_lock_management', // 门锁管理
+  GROUP_CODE_BASE = 'group_code_base', // 集团代码库
+  GROUP_ROOM_RATE = 'group_room_rate', // 集团房价
+  GROUP = 'group', // 编组
+}
