22949e20ef92e500c018fd43b75846d791c77b9f
smallsky2516 <smallsky2516@yeah.net>
2024-04-13 15:18:47 +0800

    feat(icon): svgIcon组件优化：移除svg-loder

---
+++ src/utils/isNetPath.ts
@@ -0,0 +1,1 @@
+export const isNetPath = (value: string) => /^(https?):\/\//.test(value)
--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import WindiCSS from 'vite-plugin-windicss'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,11 +15,7 @@ export default defineConfig({
     Components({
       resolvers: [AntDesignVueResolver()]
     }),
-    WindiCSS(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(__dirname, 'src/assets/icons')],
-      symbolId: 'icon-[name]'
-    })
+    WindiCSS()
   ],
   resolve: {
     alias: {
