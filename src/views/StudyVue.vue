2dcb42ebba5dc570f66af8d3e8a743d05bb1202d
smallsky2516 <smallsky2516@yeah.net>
2024-06-09 17:31:59 +0800

    feat(geo): country

--- src/views/StudyVue.vue
+++ src/views/StudyVue.vue
@@ -3,9 +3,12 @@
 </template>
 
 <script setup lang="ts">
+import { pinyin } from 'pinyin-pro'
 import { useNumberAnimation } from '@/hooks'
 
 const curNum = useNumberAnimation(291.668)
+
+console.log(pinyin('李顺祥'))
 </script>
 
 <style lang="scss" scoped></style>
